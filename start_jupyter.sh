91724d531b853d8bd8cf916afc5aa4e0d47c146d
frshd <frshd.jfri@gmail.com>
2024-06-24 20:21:11 -0400

    Use cache in building docker. Use already built image if exists.

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -12,14 +12,19 @@ HOST_DIR=$(pwd)
 echo "Dockerfile Path: $DOCKERFILE_PATH"
 echo "Host Directory: $HOST_DIR"
 
-# Build the Docker image
-echo "Building Docker image..."
-docker build -t $IMAGE_NAME -f $DOCKERFILE_PATH .
+# Check if the Docker image already exists
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+    # Build the Docker image if it doesn't exist
+    echo "Building Docker image..."
+    docker build --cache-from $IMAGE_NAME -t $IMAGE_NAME -f $DOCKERFILE_PATH .
 
-# Check if the build was successful
-if [ $? -ne 0 ]; then
-    echo "Docker build failed. Exiting."
-    exit 1
+    # Check if the build was successful
+    if [ $? -ne 0 ]; then
+        echo "Docker build failed. Exiting."
+        exit 1
+    fi
+else
+    echo "Docker image $IMAGE_NAME already exists. Skipping build."
 fi
 
 # Run the Docker container with the volume mount
